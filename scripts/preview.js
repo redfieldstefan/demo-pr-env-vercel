ae0c59df180284b48aa54a80fe1b556249798618
Stefan Redfield <redfield.stefan@gmail.com>
2022-05-16 15:08:42 -0700

    some other next command

--- scripts/preview.js
+++ scripts/preview.js
@@ -23,7 +23,7 @@ const VERCEL_CI_TOKEN = process.env.VERCEL_CI_TOKEN;
 
 const execString = `vercel --token ${VERCEL_CI_TOKEN} --build-env WHICH_ENV="${config.env}" --build-env MESSAGE="${config.message}"`;
 
-exec(execString, (error, stdout, stderr) => {
+exec("yarn build", (error, stdout, stderr) => {
   console.log("executed")
 
   if(error) {
