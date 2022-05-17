e3db84ff305c05017334fb47576f493e6cdc79db
Stefan Redfield <redfield.stefan@gmail.com>
2022-05-17 14:18:00 -0700

    get rid of link

--- scripts/preview.js
+++ scripts/preview.js
@@ -21,7 +21,7 @@ if (!config) {
 
 const VERCEL_CI_TOKEN = process.env.VERCEL_CI_TOKEN;
 
-const execString = `vercel link --debug --confirm --token ${VERCEL_CI_TOKEN} --build-env WHICH_ENV="${config.env}" --build-env MESSAGE="${config.message}"`;
+const execString = `vercel --debug --confirm --token ${VERCEL_CI_TOKEN} --build-env WHICH_ENV="${config.env}" --build-env MESSAGE="${config.message}"`;
 
 exec(execString, (error, stdout, stderr) => {
   console.log("executed")
